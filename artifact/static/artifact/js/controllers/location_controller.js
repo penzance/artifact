7aae29651b186e9d7c42d5a009b0e2679d525cac
James Curtin <jameswcurtin@gmail.com>
2015-12-17 17:13:14 -0500

    Starting to play with angular modal

--- artifact/static/artifact/js/controllers/location_controller.js
+++ artifact/static/artifact/js/controllers/location_controller.js
@@ -6,7 +6,6 @@
             'djangoUrl',
             function($scope, $http, $timeout, $djangoUrl)
 			{
-				// var mc = this;
 				var GET_LOC = $djangoUrl.reverse('artifact:map_location', [$scope.map_id]);
 				var GET_MARKERS = $djangoUrl.reverse('artifact:markers', [$scope.map_id]);
 				var GET_CSV = $djangoUrl.reverse('artifact:csv_points', [$scope.map_id]);
@@ -121,7 +120,6 @@
 				// uploads a file with multiple csv points to the map
 				$scope.uploadFile = function()
 				{
-
 					// configuration for csv parser
 					function buildConfig()
 					{
@@ -201,4 +199,114 @@
 						.modal('hide');
 				};
 			}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	angular.module('app')
+		.controller('ModalDemoCtrl', function($scope, $uibModal, $log)
+		{
+
+			$scope.items = ['item1', 'item2', 'item3'];
+
+			$scope.animationsEnabled = true;
+
+			$scope.open = function(size)
+			{
+
+				var modalInstance = $uibModal.open(
+				{
+					animation: $scope.animationsEnabled,
+					template: '<div class="modal-header"><h3 class="modal-title">Im a modal!</h3></div>',
+					controller: 'ModalInstanceCtrl',
+					size: size,
+					resolve:
+					{
+						items: function()
+						{
+							return $scope.items;
+						}
+					}
+				});
+
+				modalInstance.result.then(function(selectedItem)
+				{
+					$scope.selected = selectedItem;
+				}, function()
+				{
+					$log.info('Modal dismissed at: ' + new Date());
+				});
+			};
+
+			$scope.toggleAnimation = function()
+			{
+				$scope.animationsEnabled = !$scope.animationsEnabled;
+			};
+
+		});
+
+	// Please note that $modalInstance represents a modal window (instance) dependency.
+	// It is not the same as the $uibModal service used above.
+
+	angular.module('app')
+		.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, items)
+		{
+
+			$scope.items = items;
+			$scope.selected = {
+				item: $scope.items[0]
+			};
+
+			$scope.ok = function()
+			{
+				$uibModalInstance.close($scope.selected.item);
+			};
+
+			$scope.cancel = function()
+			{
+				$uibModalInstance.dismiss('cancel');
+			};
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })();
