66b72704ded451b7452c99e4c6cad979e3513fae
James Curtin <jameswcurtin@gmail.com>
2015-12-17 16:10:00 -0500

    cleanup and comment api.py

--- artifact/api.py
+++ artifact/api.py
@@ -1,24 +1,23 @@
-import logging
+from artifact.serializers import MapSerializer, MarkersSerializer
+from artifact.models import Map, Markers
 
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
-from django.http import HttpResponse
-
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from artifact.models import Map, Markers
-from artifact.serializers import MapSerializer, MarkersSerializer
+from django.http import HttpResponse
+from django.utils import timezone
+
 from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
-# from snippets.models import Snippet
-# from snippets.serializers import SnippetSerializer
-import csv
-import urllib
-import urllib2
+from rest_framework.response import Response
+from rest_framework import status
+
 import urlparse
+import urllib2
+import logging
+import urllib
 import json
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -32,17 +31,20 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-
-
+# Loads/saves map views from the landing page
 @login_required
 @api_view(['GET', 'POST'])
 def map_collection(request, canvas_course_id):
-    logger.debug('here')
+
     if request.method == 'GET':
+
         maps = Map.objects.filter(canvas_course_id=canvas_course_id)
         serializer = MapSerializer(maps, many=True)
+
         return Response(serializer.data)
+
     elif request.method == 'POST':
+
         logged_in_user_id = request.LTI['lis_person_sourcedid']
         data = { 'canvas_course_id': canvas_course_id,
                  'title': request.data.get('title'),
@@ -53,14 +55,18 @@ def map_collection(request, canvas_course_id):
                  'date_modified': timezone.now(),
                  'created_by': logged_in_user_id,
                  'modified_by': logged_in_user_id,
-
                  }
+
         serializer = MapSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+# Get the map that is selected
 @login_required
 @api_view(['GET'])
 def map_location(request, map_id):
@@ -74,25 +80,25 @@ def map_location(request, map_id):
         return Response(serializer.data)
 
 
+# Generate the points for the selected map and process single point upload
 @login_required
 @api_view(['GET', 'POST'])
 def marker_collection(request, map_id):
-    logger.debug('HERE')
+
     if request.method == 'GET':
+
         maps = Markers.objects.filter(map_id=map_id)
         serializer = MarkersSerializer(maps, many=True)
+
         return Response(serializer.data)
+
     elif request.method == 'POST':
-        logger.debug("ASDFJKL")
-        logger.debug('%s' % request.data);
-        # logged_in_user_id = request.LTI['lis_person_sourcedid']
         logged_in_user_id = request.user.username
         latitude = request.data.get('latitude')
         longitude = request.data.get('longitude')
         address = request.data.get('address')
-        logger.debug(address)
-        logger.debug(latitude)
-        logger.debug(longitude)
+
+        # lat/long points take precedence over addresses
         if latitude is not None and longitude is not None:
             data = {'title': request.data.get('title'),
                     'map': map_id,
@@ -106,29 +112,23 @@ def marker_collection(request, map_id):
                     'date_created': timezone.now(),
                     'date_modified': timezone.now(),
                     }
-            logger.debug("PRINT DATA")
-            logger.debug(data)
+
             serializer = MarkersSerializer(data=data)
+
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+
             else:
-                logger.debug(serializer.errors)
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         elif address:
-            logger.debug("WENTINTOADDRESS")
             address = urllib.quote(address.encode("utf-8"))
-            logger.debug(address)
             url = 'http://maps.googleapis.com/maps/api/geocode/json?address='+address+'&sensor=true'
             data = urllib2.urlopen(url).read()
             json_data = json.loads(data)
             stat = json_data.get('status')
-            print
-            print
-            logger.debug(json_data)
-            print
-            print
-            print '{}'.format(stat)
+
             if stat in 'OK':
                 result = json_data['results'][0]         
                 latitude = result['geometry']['location']['lat']
@@ -146,98 +146,51 @@ def marker_collection(request, map_id):
                         'date_created': timezone.now(),
                         'date_modified': timezone.now(),
                         }
-                logger.debug("PRINT DATA")
-                logger.debug(data)
+
                 serializer = MarkersSerializer(data=data)
                 if serializer.is_valid():
                     serializer.save()
-                    print '{}'.format(json.dumps({'latitude': latitude, 'longitude': longitude}))
                     return Response({'latitude': latitude, 'longitude': longitude}, status=status.HTTP_201_CREATED)
                 else:
-                    logger.debug(serializer.errors)
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
             else:
                 errors = ['Address not found!']
                 return JSONResponse(errors, status=status.HTTP_400_BAD_REQUEST)
+        
         else:
             errors = ['You did not enter either an address or lat/long. Try again.']
             return JSONResponse(errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Uploading multiple points via CSV file
 @login_required
-@api_view(['GET','POST'])
+@api_view(['POST'])
 def csv_points(request, map_id):
-    logged_in_user_id = request.user.username
-    # for i in request.data.dict().keys():
-    #     logger.debug(i)
-    datatouse = request.data.dict().keys()[0]
-    datatouse = json.loads(datatouse)
-    errors = []
-    for item in datatouse:
-        logger.debug(type(item))
-        logger.debug(item)
-        data = {'title': item[u'title'],
-                'map': map_id,
-                'latitude': item[u'latitude'],
-                'longitude': item[u'longitude'],
-                'description': item[u'description'],
-                'external_url': item[u'externalurl'],
-                'created_by': logged_in_user_id,
-                'modified_by': logged_in_user_id,
-                'date_created': timezone.now(),
-                'date_modified': timezone.now(),
-                }
-        serializer = MarkersSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            # return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == 'POST':
+        logged_in_user_id = request.user.username
+        datatouse = request.data.dict().keys()[0]
+        datatouse = json.loads(datatouse)
+        errors = []
+        for item in datatouse:
+            data = {'title': item[u'title'],
+                    'map': map_id,
+                    'latitude': item[u'latitude'],
+                    'longitude': item[u'longitude'],
+                    'description': item[u'description'],
+                    'external_url': item[u'externalurl'],
+                    'created_by': logged_in_user_id,
+                    'modified_by': logged_in_user_id,
+                    'date_created': timezone.now(),
+                    'date_modified': timezone.now(),
+                    }
+
+            serializer = MarkersSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                errors.append(serializer.errors)
+        if len(errors) != 0:
+            return JSONResponse(errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            #logger.debug(serializer.errors)
-            #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            errors.append(serializer.errors)
-    if len(errors) != 0:
-        logger.debug (errors)
-        return JSONResponse(errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # # logged_in_user_id = request.LTI['lis_person_sourcedid']
-    # logged_in_user_id = request.user.username
-    # data = {'title': request.data.get('title'),
-    #         'map': map_id,
-    #         'latitude': request.data.get('latitude'),
-    #         'longitude': request.data.get('longitude'),
-    #         'description': request.data.get('description'),
-    #         'external_url': request.data.get('externalurl'),
-    #         'fileupload': request.data.get('fileupload'),
-    #         'created_by': logged_in_user_id,
-    #         'modified_by': logged_in_user_id,
-    #         'date_created': timezone.now(),
-    #         'date_modified': timezone.now(),
-    #         }
-    # logger.debug("PRINT DATA")
-    # logger.debug(data)
-    # serializer = MarkersSerializer(data=data)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    # else:
-    #     logger.debug(serializer.errors)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
